1cd051607a8cb7133cb138e35d6b71e843e1db11
piterds7-blip <piterds7@gmail.com>
2026-09-16 09:51:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='stuchowo-v32';
+const CACHE_NAME='stuchowo-v35';
 const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const CDN=[
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
